d09b81c95405a79a443b093f602d47efb18491e6
SETHUPATHI.S <144593453+SETHUPATHI77@users.noreply.github.com>
2025-03-22 18:43:59 +0530

    Update API.py

--- API.py
+++ API.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -18,4 +19,5 @@ def fetch_data():
     return jsonify(latest_data)
 
 if __name__ == '__main__':
-    app.run()  # Ensure it's accessible on the network
+    port = int(os.environ.get("PORT", 5000))  # Use Render's assigned port
+    app.run(host='0.0.0.0', port=port)
